610f90db854bc101fed303d23e6d65dabb5e5e7b
Gabriel Augustin <gabriel.augustin@edu.sc.senai.br>
2024-02-28 22:05:03 -0300

    feat: Criação da regra de negócio

--- src/main/java/br/com/fullstack/education/controller/CursoController.java
+++ src/main/java/br/com/fullstack/education/controller/CursoController.java
@@ -1,24 +1,42 @@
 package br.com.fullstack.education.controller;
 
+import br.com.fullstack.education.model.Curso;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("cursos")
 public class CursoController {
 
+    /** Buscar todos */
     @GetMapping
-    public String get(Integer id) {
-        return "GET /cursos com id (" + id + ")";
+    public List<Curso> get() {
+        return Curso.getCursosCadastrados();
     }
 
+    /** Busca por ID */
     @GetMapping("{id}")
-    public String getId(@PathVariable Integer id) {
-        return "GET /cursos/" + id;
+    public Curso getId(@PathVariable Integer id) throws Exception {
+        return Curso.buscarPorId(id);
     }
 
+    /** Criação */
     @PostMapping
-    public String post() {
-        return "POST /cursos IHUL ";
+    public Curso post(@RequestBody Curso curso) throws Exception {
+        return Curso.salvar(curso);
+    }
+
+    /** Edição */
+    @PutMapping("{id}")
+    public Curso put(@PathVariable Integer id, @RequestBody Curso curso) throws Exception {
+        return Curso.salvar(id, curso);
+    }
+
+    /** Exclusão */
+    @DeleteMapping("{id}")
+    public String delete(@PathVariable Integer id) {
+        return "DELETE /cursos/" + id;
     }
 
 }
